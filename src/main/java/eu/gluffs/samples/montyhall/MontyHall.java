111bb9481e781331f55e2ae91b52fabc67f8d796
Joakim Lundin <joakim.lundin@su.se>
2013-11-20 21:08:48 +0100

    Added javadoc & renamed constant

--- src/main/java/eu/gluffs/samples/montyhall/MontyHall.java
+++ src/main/java/eu/gluffs/samples/montyhall/MontyHall.java
@@ -1,27 +1,33 @@
 package eu.gluffs.samples.montyhall;
 
 public final class MontyHall {
-  private static final int GAMES_PER_TEST = 1000;
+  /**
+   * Number of games to run per game runner
+   */
+  private static final int GAMES_PER_RUNNER = 1000;
 
   private GameRunner change;
   private GameRunner noChange;
 
+  /**
+   * Sets up game runners for this test.
+   */
   private MontyHall() {
-    change = new GameRunner(GAMES_PER_TEST);
-    noChange = new GameRunner(GAMES_PER_TEST);
+    change = new GameRunner(GAMES_PER_RUNNER);
+    noChange = new GameRunner(GAMES_PER_RUNNER);
   }
 
-  public static void main(String[] args) {
-    MontyHall montyHall = new MontyHall();
-    montyHall.play();
-    montyHall.printResult();
-  }
-
+  /**
+   * Runs the test.
+   */
   private void play() {
     change.play(true);
     noChange.play(false);
   }
 
+  /**
+   * Prints the test result.
+   */
   private void printResult() {
     StringBuilder sb = new StringBuilder();
     sb.append("Changing box is ");
@@ -38,6 +44,16 @@ public final class MontyHall {
     sb.append("% wins.");
 
     System.out.println(sb.toString());
+  }
 
+  /**
+   * Starts the test.
+   *
+   * @param args accepts no parameters
+   */
+  public static void main(String[] args) {
+    MontyHall montyHall = new MontyHall();
+    montyHall.play();
+    montyHall.printResult();
   }
 }
