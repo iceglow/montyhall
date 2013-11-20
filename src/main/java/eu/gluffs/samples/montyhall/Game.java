d756cc0671e0ff75585097ce82e8f967e3bfc04b
Joakim Lundin <joakim.lundin@su.se>
2013-11-20 20:26:29 +0100

    Added some javadoc.

--- src/main/java/eu/gluffs/samples/montyhall/Game.java
+++ src/main/java/eu/gluffs/samples/montyhall/Game.java
@@ -1,13 +1,24 @@
 package eu.gluffs.samples.montyhall;
 
+/**
+ * A monty Hall game that can be played with or without changing box after the initial guess.
+ */
 public class Game {
   private boolean[] boxes = {false, true, false};
   private int guess;
 
+  /**
+   * Creates a new Game and records the first guess.
+   *
+   * @param guess the box number to guess for 0-2.
+   */
   public Game(int guess) {
     this.guess = guess;
   }
 
+  /**
+   * Changes the guess to another box.
+   */
   public void changeBox() {
     if (guess == 1)
       guess = 0;
@@ -15,6 +26,11 @@ public class Game {
       guess = 1;
   }
 
+  /**
+   * Checks whether the guess was correct.
+   *
+   * @return true for a correct guess & false for an incorrect guess.
+   */
   public boolean isCorrect() {
     return boxes[guess];
   }
--- src/main/java/eu/gluffs/samples/montyhall/GameRunner.java
+++ src/main/java/eu/gluffs/samples/montyhall/GameRunner.java
@@ -4,10 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Plays a number of games to figure out the win percentage.
+ */
 public class GameRunner {
   private List<Game> games;
   private int wins = 0;
 
+  /**
+   * Creates a new game runner with the supplied number of games
+   *
+   * @param numberOfGames number of games to run.
+   */
   public GameRunner(int numberOfGames) {
     games = new ArrayList<Game>(numberOfGames);
 
@@ -16,6 +24,11 @@ public class GameRunner {
     }
   }
 
+  /**
+   * Plays all the games in the runner.
+   *
+   * @param changeBox true to change box after the initial guess, false to keep the initial guess.
+   */
   public void play(boolean changeBox) {
     for (Game game : games) {
       if(changeBox)
@@ -26,14 +39,28 @@ public class GameRunner {
     }
   }
 
+  /**
+   * Return the number of wins for this runner.
+   *
+   * @return the number of wins.
+   */
   public int getWins() {
     return this.wins;
   }
 
+  /**
+   * Returns the win percentage for the games in this runner.
+   *
+   * @return the percentage of wins.
+   */
   public int winPercent() {
     return (int) ((float) wins / (float) games.size() * 100);
   }
 
+  /**
+   * @see Object#toString()
+   */
+  @Override
   public String toString() {
     return winPercent() + "% wins";
   }
